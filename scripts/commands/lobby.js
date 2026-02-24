9521cfd67ba0400e9b0293eb872d51f637d8a3cf
zbike9618 <zealousbike9618@gmail.com>
2026-02-24 16:04:17 +0900

    a

--- scripts/commands/lobby.js
+++ scripts/commands/lobby.js
@@ -33,7 +33,7 @@ function DoCommand(origin) {
     const player = origin.sourceEntity;
     //関数を実行する
     system.run(() => {
-        player.teleport({ x: 0, y: 65, z: 0 })
+        player.teleport({ x: 0, y: 65, z: 0, dimension: "overworld" })
         player.sendMessage({ translate: "cw.phone.went.lobby" })
     })
 
