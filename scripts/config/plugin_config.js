59700180ea295ee1053e98ecd51f330c5f789c3e
CreamBread868 <tomio.miyazaki@gmail.com>
2026-03-11 17:21:42 +0900

    検知

--- scripts/config/plugin_config.js
+++ scripts/config/plugin_config.js
@@ -7,6 +7,12 @@ export const turnedOnPlugins = {
     "admin": {
         blackList: [
             "test"
+        ],
+        opWhiteList: [
+            "test"
+        ],
+        creativeWhiteList: [
+            "test"
         ]
     },
     "block_logger": {},
--- scripts/plugins/admin/import.js
+++ scripts/plugins/admin/import.js
@@ -1,4 +1,6 @@
 import { turnedOnPlugins } from "../../config/plugin_config";
 export const blacklist = turnedOnPlugins["admin"].blackList;
+export const opWhiteList = turnedOnPlugins["admin"].opWhiteList;
+export const creativeWhiteList = turnedOnPlugins["admin"].creativeWhiteList;
 import "./commands";
 import "./system"
