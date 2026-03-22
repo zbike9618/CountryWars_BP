5ca925441c9635734b8a3f60e0e1d1def78b41dc
CreamBread868 <tomio.miyazaki@gmail.com>
2026-03-22 19:39:54 +0900

    a

--- scripts/plugins/admin/interval.js
+++ scripts/plugins/admin/interval.js
@@ -1,6 +1,5 @@
 import * as server from "@minecraft/server";
 const { world, system } = server;
-import { Lore } from "../../utils/lore";
 const ids = "qwertyuiopasdfghjklzxcvbnm1234567890"
 function oriId() {
     let ori = "";
@@ -13,31 +12,36 @@ function oriId() {
 }
 system.runInterval(() => {
     for (const player of world.getAllPlayers()) {
-        try {
-            const comp = player.getComponent("inventory");
-            const inv = comp.container;
-            const has = []
-            for (let i = 0; i < inv.size; i++) {
-                const item = inv.getItem(i);
-                if (item) {
-                    let key = Lore.getLore(player, i, "id:");
 
-                    if (!key) {
-                        const newId = oriId();
-                        Lore.setLore(player, i, "id:", newId);
-                        key = newId;
-                    }
+        const comp = player.getComponent("inventory");
+        const inv = comp.container;
+        const has = []
+        for (let i = 0; i < inv.size; i++) {
+            const item = inv.getItem(i);
+            if (item) {
+                if (item.maxAmount != 1) continue;
+                let key = item.getDynamicProperty("cw:id");
 
-                    if (has.includes(key)) {
-                        //player.runCommand("kick @s 不正なアイテムの増殖");
-                        inv.setItem(i);
-                        continue;
-                    }
-                    has.push(key);
+                if (key === undefined) {
+                    const newId = oriId();
+                    item.setDynamicProperty("cw:id", newId);
+
+                    inv.setItem(i, item);
+
+                    const Haveitem = inv.getItem(i);
+                    key = Haveitem ? Haveitem.getDynamicProperty("cw:id") : newId;
+
+                    world.sendMessage(`発番: ${key}`);
                 }
+
+                if (has.includes(key)) {
+                    //player.runCommand("kick @s 不正なアイテムの増殖");
+                    inv.setItem(i);
+                    continue;
+                }
+                has.push(key);
             }
-        } catch (error) {
-            world.sendMessage(error);
         }
     }
+
 })
