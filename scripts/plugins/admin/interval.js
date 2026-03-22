1443f66818bd82bd8668649800bbb508169f53eb
CreamBread868 <tomio.miyazaki@gmail.com>
2026-03-22 19:22:46 +0900

    e

--- scripts/plugins/admin/interval.js
+++ scripts/plugins/admin/interval.js
@@ -12,33 +12,37 @@ function oriId() {
 }
 system.runInterval(() => {
     for (const player of world.getAllPlayers()) {
-        const comp = player.getComponent("inventory");
-        const inv = comp.container;
-        const has = []
-        for (let i = 0; i < inv.size; i++) {
-            const item = inv.getItem(i);
-            if (item) {
-                let key = item.getDynamicProperty("cw:id");
+        try {
+            const comp = player.getComponent("inventory");
+            const inv = comp.container;
+            const has = []
+            for (let i = 0; i < inv.size; i++) {
+                const item = inv.getItem(i);
+                if (item) {
+                    let key = item.getDynamicProperty("cw:id");
 
-                if (key === undefined) {
-                    const newId = oriId();
-                    item.setDynamicProperty("cw:id", newId);
+                    if (key === undefined) {
+                        const newId = oriId();
+                        item.setDynamicProperty("cw:id", newId);
 
-                    inv.setItem(i, item);
+                        inv.setItem(i, item);
 
-                    const Haveitem = inv.getItem(i);
-                    key = Haveitem ? Haveitem.getDynamicProperty("cw:id") : newId;
+                        const Haveitem = inv.getItem(i);
+                        key = Haveitem ? Haveitem.getDynamicProperty("cw:id") : newId;
 
-                    world.sendMessage(`発番: ${key}`);
+                        world.sendMessage(`発番: ${key}`);
+                    }
+
+                    if (has.includes(key)) {
+                        //player.runCommand("kick @s 不正なアイテムの増殖");
+                        inv.setItem(i);
+                        continue;
+                    }
+                    has.push(key);
                 }
-
-                if (has.includes(key)) {
-                    //player.runCommand("kick @s 不正なアイテムの増殖");
-                    inv.setItem(i);
-                    continue;
-                }
-                has.push(key);
             }
+        } catch (error) {
+            console.error(error);
         }
     }
 })
